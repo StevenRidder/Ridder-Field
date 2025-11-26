0ae840d1616bc321122b240be9a344588d482cdf
steveridder <steveridder@example.com>
2025-11-25 18:07:08 -1000

    Fix file locking issue in v3 tier3 test chains

--- phase3/scripts/run_v3_tier3_test.sh
+++ phase3/scripts/run_v3_tier3_test.sh
@@ -21,6 +21,8 @@ echo "Cleaning previous Tier 3 test chains..."
 rm -rf $CHAINS_DIR/v3_tier3_test_chain*_work
 rm -f $CHAINS_DIR/v3_tier3_test_chain*.txt
 rm -f $CHAINS_DIR/v3_tier3_test_chain*.updated.yaml
+rm -f $CHAINS_DIR/v3_tier3_test*.info.yaml
+rm -f $CHAINS_DIR/v3_tier3_test*.lock
 
 # Launch 4 chains (each in isolated directory)
 echo ""
@@ -34,7 +36,7 @@ for i in {1..4}; do
     sed -i "s|output: chains/v3_tier3_test|output: $CHAINS_DIR/v3_tier3_test_chain${i}|g" $WORK_DIR/config.yaml
     
     cd $WORK_DIR
-    nohup cobaya-run config.yaml --force > chain${i}.log 2>&1 &
+    COBAYA_USE_FILE_LOCKING=False nohup cobaya-run config.yaml --force > chain${i}.log 2>&1 &
     PID=$!
     echo "  Chain ${i}: PID ${PID} in ${WORK_DIR}"
     cd - > /dev/null
